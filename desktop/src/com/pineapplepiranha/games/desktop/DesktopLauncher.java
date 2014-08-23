f4a351822d4667cb47e7aa4a427fa7714d16dffd
Barryrowe <barryrowe@gmail.com>
2014-08-23 00:01:12 -0400

    Initial Commit for ludum dare
    Adding the initial project

---
+++ desktop/src/com/pineapplepiranha/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pineapplepiranha.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pineapplepiranha.games.CityLightsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CityLightsGame(), config);
+	}
+}
